e0923bbf82b6770a6f6adb6e676d327442f3ebce
jhigs <flores.gideon1@gmail.com>
2021-03-04 09:19:36 +0800

    Add mapping and makeCard function

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function Detail(props) {
-  console.log(props);
   return <p className='info'>{props.detail}</p>;
 }
 
---
+++ src/makeCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './components/Card';
+
+function makeCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
+export default makeCard;
